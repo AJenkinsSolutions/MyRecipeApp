24953115b505d755b6e0153cfaf8c098aa137a99
AJenkinsSolutions <ajenkinsuiw@gmail.com>
2023-07-10 14:54:02 -0500

    Add page structure, Add NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { IngredientComponent } from './ingredient/ingredient.component';
 
-import { AppHeaderComponent } from './app-header/app-header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+
 
 
 
@@ -19,11 +20,13 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     AppComponent,
     ShoppingListComponent,
     IngredientComponent,
-    AppHeaderComponent,
     RecipesComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingEditComponent,
+    AppHeaderComponent,
+    RecipeListComponent
+    
   ],
   imports: [
     BrowserModule
