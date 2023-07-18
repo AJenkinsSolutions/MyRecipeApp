ed3b746bd7f42485de7463a1b04dd987de1937ae
AJenkinsSolutions <ajenkinsuiw@gmail.com>
2023-07-18 12:09:43 -0500

    Add list components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class RecipeListComponent {
 
+  //This array only hold recipe objects
+  recipes: Recipe[] = [
+    new Recipe('test-name', 'test-Description', 'https://www.cookingclassy.com/wp-content/uploads/2018/09/beef-stroganoff-14.jpg'),
+    new Recipe('test-name2', 'test-Description2', 'https://www.cookingclassy.com/wp-content/uploads/2018/09/beef-stroganoff-14.jpg'),
+    new Recipe('test-name', 'test-Description3', '')
+  ];
+
+  constructor(){};
+
+  ngOnInit(){};
+
+  
 }
